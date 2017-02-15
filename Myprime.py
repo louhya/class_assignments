0922f442215364d6832649bf73e2eec95478b84c
Louis <lhyacinthe@gmail.com>
2017-02-14 20:39:14 -0500

    upload two 2/11/2017

--- Myprime.py
+++ Myprime.py
@@ -1,9 +1,9 @@
-PrimeList = []
+PrimeList = [2, 3, 5, 7,]
 num = int(input('Please enter a number?'))
 for i in range(3, num+1, 2):    
     if i >= 2:
         if i % 2 != 0 and i % 3 != 0 and i % 5 != 0 and i % 7 != 0:
             PrimeList.append(i)
-            
+
 else:
     print (PrimeList) 
--- lambda_prime.py
+++ lambda_prime.py
@@ -3,3 +3,5 @@ def Opt_primes(n=int(input('Please enter a number: '))): # simple Sieve of Erato
     sieve = set(sum([list(range(q*q, n+1, q+q)) for q in odds],[]))
     print ([2] + [p for p in odds if p not in sieve])
 Opt_primes()
+
+
