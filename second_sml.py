905ae387bfff7d82fbb95e336b453307b70c955a
Louis <lhyacinthe@gmail.com>
2017-03-11 15:58:22 -0500

    upload 3/11/2017

---
+++ second_sml.py
@@ -0,0 +1,19 @@
+# 0, 42, 8, 13, 79, 5, 2, 18
+Xlist = input("Please enter all the numbers in your list separated by one space: ").split(',')
+XArray = []
+Xlist_size = len(Xlist)
+print(Xlist_size)
+print('max_Xlist', max(Xlist))
+for x in range(Xlist_size):
+    Xlval = int(Xlist[x])
+    print('Xval', Xlval)
+x += 1
+#min1st = min2nd = 0
+#print(min1st, min2nd)
+#minimum = Xlist[0]  # arbitrary number in list
+#    for x in Xlist:
+ #       if x < minimum:
+  #          minimum = x
+   # new_list.append(minimum)
+    #Xlist.remove(minimum)
+
